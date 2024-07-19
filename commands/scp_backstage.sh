7e30d09cfb87d1231e423ef140962306edfd1831
良奈 <lunifans@outlook.com>
2024-07-19 09:35:18 +0800

    方便将scp 放到后台运行的脚本

---
+++ commands/scp_backstage.sh
@@ -0,0 +1,12 @@
+echo -e "\033[36m将 scp 放到后台的脚本，作者：Lunifans\033[0m"
+echo "请输入源文件夹路径"
+read sourcePath
+echo "请输入用户名@主机名,例如 root@ip地址"
+read userAtHost
+echo "请输入目标文件夹路径"
+read targetPath
+echo "程序运行后，Ctrl+Z 放到后台运行"
+ssh-keygen -t rsa
+ssh-copy-id $userAtHost
+nohup scp -r $sourcePath $userAtHost:$targetPath&
+echo -e "\033[36m耐心等待文件传输完毕！\033[0m"
